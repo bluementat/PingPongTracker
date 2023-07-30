db883c325a484572360926d48a069632898d522e
Michael Ingersoll <michael@michaelcoding.com>
2023-07-30 16:44:26 -0400

    Created Testing Project - Creating a tests folder for the project.

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PingPongTracker.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
