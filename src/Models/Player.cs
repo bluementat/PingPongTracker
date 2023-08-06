701b24e42d4b0b6d2723a1ebaacfca1a1148d90e
Michael Ingersoll <michael@michaelcoding.com>
2023-08-06 12:08:49 -0400

    Initial work on new Players Class

---
+++ src/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace PingPongTracker.Models;
+
+public class Player
+{
+    public Guid PlayerId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int TotalGames { get; set; }
+    public int TotalPoints { get; set; }    
+}
