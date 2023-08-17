dbb1b3a68623b3cccb9cc3407d667e3ef04fb370
Michael Ingersoll <michael@michaelcoding.com>
2023-08-16 20:46:10 -0400

    Create Matchup, Tournament, and Season Entities Fixes #66 Renamed Matchup to Games

---
+++ src/Migrations/AppMigrations/20230817003645_ApplicationEntities.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PingPongTracker.migrations.AppMigrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TournamentId",
+                table: "Players",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Seasons",
+                columns: table => new
+                {
+                    SeasonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SeasonStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SeasonEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seasons", x => x.SeasonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tournaments",
+                columns: table => new
+                {
+                    TournamentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TournamentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SeasonId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tournaments", x => x.TournamentId);
+                    table.ForeignKey(
+                        name: "FK_Tournaments_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "SeasonId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    MatchupId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Team1Player1Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Team1Player2Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Team2Player1Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Team2Player2Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Team1Score = table.Column<int>(type: "int", nullable: false),
+                    Team2Score = table.Column<int>(type: "int", nullable: false),
+                    MatchupDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Player1WinnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Player2WinnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TournamentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.MatchupId);
+                    table.ForeignKey(
+                        name: "FK_Games_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "TournamentId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TournamentId",
+                table: "Players",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_TournamentId",
+                table: "Games",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tournaments_SeasonId",
+                table: "Tournaments",
+                column: "SeasonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Tournaments_TournamentId",
+                table: "Players",
+                column: "TournamentId",
+                principalTable: "Tournaments",
+                principalColumn: "TournamentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Tournaments_TournamentId",
+                table: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Tournaments");
+
+            migrationBuilder.DropTable(
+                name: "Seasons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_TournamentId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TournamentId",
+                table: "Players");
+        }
+    }
+}
---
+++ src/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PingPongTracker.Models;
+
+public class Game
+{
+    [Key]
+    public Guid MatchupId { get; set; }
+    [Required]
+    public Guid Team1Player1Id { get; set; }
+    [Required]
+    public Guid Team1Player2Id { get; set; }
+    [Required]
+    public Guid Team2Player1Id { get; set; }
+    [Required]
+    public Guid Team2Player2Id { get; set; }
+    [Required]
+    public int Team1Score { get; set; }
+    [Required]
+    public int Team2Score { get; set; }
+    [Required]
+    public DateTime MatchupDate { get; set; } = DateTime.Now;
+    [Required]
+    public Guid Player1WinnerId { get; set; }
+    [Required]
+    public Guid Player2WinnerId { get; set; }
+}
