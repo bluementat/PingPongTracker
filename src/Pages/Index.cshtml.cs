560cd62c45e8dd718a923e759e563c6a11455ff6
Michael Ingersoll <mingersoll@bluecraft.onmicrosoft.com>
2023-08-18 14:51:19 -0400

    Add Standings Chart to the Main Page Fixes #68

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PingPongTracker.Data;
+using PingPongTracker.Models;
+using PingPongTracker.Pages.Admin;
 
 namespace PingPongTracker.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ApplicationDbContext _context;
 
     public string Message { get; set; } = string.Empty;
+    public IEnumerable<PlayerStandingViewModel> Players { get; set; } = new List<PlayerStandingViewModel>();
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext Context)
     {
+        _context = Context;
         _logger = logger;
     }
 
@@ -21,5 +26,33 @@ public class IndexModel : PageModel
         var greetings = Greetings.GetGreetings().ToList();
         var greeting = greetings[new Random().Next(0, greetings.Count)];
         Message = greeting.Text;
+
+        IEnumerable<PlayerStandingViewModel> PreSort = new List<PlayerStandingViewModel>();
+
+        // Get all active players
+        var players = _context.Players.Where(p => p.Active).ToList();
+
+        foreach(var player in players)
+        {
+            var wins = _context.Games.Where(g => g.Player1WinnerId == player.PlayerId).Count();
+            wins += _context.Games.Where(g => g.Player2WinnerId == player.PlayerId).Count();            
+            var totalGames = _context.Games.Where(g => g.Team1Player1Id == player.PlayerId || g.Team1Player2Id == player.PlayerId 
+                || g.Team2Player1Id == player.PlayerId || g.Team2Player2Id == player.PlayerId).Count();
+            var winPercentage = totalGames == 0 ? 0 : (int)Math.Round((double)wins / totalGames * 100);
+            var losses = totalGames - wins;
+
+            PreSort = PreSort.Append(new PlayerStandingViewModel
+            {
+                Rank = 0,
+                UserName = player.UserName,
+                Wins = wins,
+                Losses = losses,
+                TotalGames = totalGames,
+                WinPercentage = winPercentage
+            });
+        }
+
+        Players = PreSort.OrderBy(p => p.WinPercentage).ThenBy(p => p.Wins).ThenBy(p => p.Losses).ToList();
+
     }
 }
