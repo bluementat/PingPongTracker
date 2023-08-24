d5fc83027e199ccc60186c81a13d31803f2d2626
Michael Ingersoll <michael@michaelcoding.com>
2023-08-24 14:53:15 +0000

    Test

---
+++ src/Data/ITournamentRepository.cs
@@ -0,0 +1,12 @@
+﻿using PingPongTracker.Models;
+
+namespace PingPongTracker.Data;
+
+public interface ITournamentRepository
+{
+    Task<Tournament> GetTournamentAsync(int id);
+    Task<IEnumerable<Tournament>> GetTournamentsAsync();
+    Task<Tournament> AddTournamentAsync(Tournament tournament);
+    Task<Tournament> UpdateTournamentAsync(Tournament tournament);
+    Task<Tournament> DeleteTournamentAsync(int id);
+}
