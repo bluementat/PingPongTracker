d1ad6526bc7dbb4d3682180fe528d51b58f4e5ab
Michael Ingersoll <michael@michaelcoding.com>
2023-09-19 21:20:22 -0400

    Allow Tournament Complete to Update Games Table Fixes #105

---
+++ src/Migrations/AppMigrations/20230920005935_ExpandGames.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PingPongTracker.Migrations.AppMigrations
+{
+    /// <inheritdoc />
+    public partial class ExpandGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Team1Name",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Team2Name",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Team1Name",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Team2Name",
+                table: "Games");
+        }
+    }
+}
